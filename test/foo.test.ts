4f0ae736ed984c3e55c5cb8d7a96842b57fcd919
Bob Gregory <bob.gregory@cazoo.co.uk>
2021-09-28 12:23:02 +0100

    Rename journey to taps in tests

--- test/foo.test.ts
+++ test/foo.test.ts
@@ -1,32 +1,32 @@
 import { Station, bill } from "../lib";
 
 describe("A single journey in zone A", () => {
-  const journey: Array<Station> = [Station.Asterisk, Station.Aldgate];
+  const taps: Array<Station> = [Station.Asterisk, Station.Aldgate];
 
   it("should charge £2.50 for the journey", () => {
-    const [charge] = bill(journey);
+    const [charge] = bill(taps);
     expect(charge.amount).toEqual(250);
   });
 });
 
 describe("One-Way Zone 1 to Zone 2 Journey", () => {
-  const journey: Array<Station> = [Station.Asterisk, Station.Barbican];
+  const taps: Array<Station> = [Station.Asterisk, Station.Barbican];
 
   it("should charge £3.00 for the journey", () => {
-    const [charge] = bill(journey);
+    const [charge] = bill(taps);
     expect(charge.amount).toEqual(300);
   });
 });
 
 describe("Multiple journeys", () => {
-  const journeys: Array<Station> = [
+  const taps: Array<Station> = [
     Station.Asterisk,
     Station.Aldgate,
     Station.Asterisk,
     Station.Balham,
   ];
 
-  const charges = Array.from(bill(journeys));
+  const charges = Array.from(bill(taps));
 
   it("should issue two charges", () => {
     expect(charges).toHaveLength(2);
@@ -34,7 +34,7 @@ describe("Multiple journeys", () => {
 });
 
 describe("Multiple journeys including zone B reaching daily cap", () => {
-  const journeys: Array<Station> = [
+  const taps: Array<Station> = [
     Station.Asterisk,
     Station.Barbican,
     Station.Barbican,
@@ -45,7 +45,7 @@ describe("Multiple journeys including zone B reaching daily cap", () => {
     Station.Asterisk,
   ];
 
-  const charges = Array.from(bill(journeys));
+  const charges = Array.from(bill(taps));
 
   it("should charge 2.00 for his third journey", () => {
     expect(charges[2].amount).toEqual(200);
@@ -53,7 +53,7 @@ describe("Multiple journeys including zone B reaching daily cap", () => {
 });
 
 describe("Multiple journeys in zone A reaching daily cap", () => {
-  const journeys: Array<Station> = [
+  const taps: Array<Station> = [
     Station.Asterisk,
     Station.Aldgate,
     Station.Aldgate,
@@ -64,7 +64,7 @@ describe("Multiple journeys in zone A reaching daily cap", () => {
     Station.Asterisk,
   ];
 
-  const charges = Array.from(bill(journeys));
+  const charges = Array.from(bill(taps));
 
   it("should charge 2.00 for his third journey", () => {
     expect(charges[2].amount).toEqual(200);
@@ -72,10 +72,19 @@ describe("Multiple journeys in zone A reaching daily cap", () => {
 });
 
 describe("When a journey starts in zone B and ends in zone A", () => {
-  const journey = [Station.Bison, Station.Aldgate];
+  const taps = [Station.Bison, Station.Aldgate];
 
   it("should charge 3.00", () => {
-    const [charge] = bill(journey);
+    const [charge] = bill(taps);
+    expect(charge.amount).toEqual(300);
+  });
+});
+
+describe("Multiple return journeys", () => {
+  const taps = [Station.Bison, Station.Aldgate];
+
+  it("should charge 3.00", () => {
+    const [charge] = bill(taps);
     expect(charge.amount).toEqual(300);
   });
 });
